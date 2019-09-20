821106cbd12c7a87dbe65a85ba98c570cff00a44
Joe Savage <joe.savage@gmail.com>
2019-09-20 07:15:13 -0500

    added RPiMAX6675TempProvider

--- PidService/temp_providers.py
+++ PidService/temp_providers.py
@@ -1,3 +1,4 @@
+import math
 
 __all__ = ["get_temp_provider_factory", "MockTempProvider"]
 
@@ -19,14 +20,41 @@ class MockTempProvider(TempProviderBase):
         print('{} temp = {}'.format(self.label, self.temp))
         return self.temp
 
+#constants
+SPI_BYTES_TO_READ = 2
+
 class RPiMAX6675TempProvider(TempProviderBase):
+
 
     def __init__(self, label="Oven"):
         self.temp = 0.0
         super().__init__(label)
+        import pigpio
+        self.pi = pigpio.pi()
+
+        if not self.pi.connected:
+            raise OSError("unable to connect to pigpio")
+        # pi.spi_open(0, 1000000, 0)   # CE0, 1Mbps, main SPI
+        # pi.spi_open(1, 1000000, 0)   # CE1, 1Mbps, main SPI
+        # pi.spi_open(0, 1000000, 256) # CE0, 1Mbps, auxiliary SPI
+        # pi.spi_open(1, 1000000, 256) # CE1, 1Mbps, auxiliary SPI
+        # pi.spi_open(2, 1000000, 256) # CE2, 1Mbps, auxiliary SPI
+        self.sensor = self.pi.spi_open(0, 1000000, 0)
 
     def read_temp(self):
-        #print('temp = {}'.format(self.temp))
+        for i in range(0, 5):
+
+            c, d = self.pi.spi_read(self.sensor, SPI_BYTES_TO_READ)
+            if c == 2:
+                word = (d[0] << 8) | d[1]
+                if (word & 0x8006) == 0:  # Bits 15, 2, and 1 should be zero.
+                    t = (word >> 3) / 4.0
+                    self.temp = t * 1.8 + 32
+                    return self.temp
+                else:
+                    continue
+        # unable to read after 5 tries
+        self.temp = float('nan')
         return self.temp
 
 class TempProviderFactory(object):
