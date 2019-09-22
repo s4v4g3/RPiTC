ff91a5858b3db2d4b1f6db4d4497fcfa962e647e
Joe Savage <joe.savage@gmail.com>
2019-09-22 14:55:28 -0500

    updated hostname for db

--- webserver/routes/data.js
+++ webserver/routes/data.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mysql = require('mysql')
 
 var conn = mysql.createConnection({
-    host: "10.4.1.240",
+    host: "localhost",
     user: "bbq",
     password: "bbq",
     database: "bbq"
@@ -14,17 +14,22 @@ var conn = mysql.createConnection({
 router.get('/', function(req, res, next) {
     secs = req.query.sec
   conn.connect(function(err) {
-      let date = Math.floor(new Date() / 1000)
-      let tzOffset = (new Date()).getTimezoneOffset() * 60
-      let localTime = (date - tzOffset)
-      let startDate = (localTime) - secs
-      let select = `SELECT * from pid_view where local_time > ${startDate} ORDER BY time asc`
-      console.log(select)
-    conn.query(select, function (err, result, fields){
-        data = result.map(a => {})
-        res.status(200).json(result)
-    })
-  });
+      if (err){
+          res.status(200).json({})
+      }
+      else {
+        let date = Math.floor(new Date() / 1000)
+        let tzOffset = (new Date()).getTimezoneOffset() * 60
+        let localTime = (date - tzOffset)
+        let startDate = (localTime) - secs
+        let select = `SELECT * from pid_view where local_time > ${startDate} ORDER BY time asc`
+        console.log(select)
+        conn.query(select, function (err, result, fields){
+            data = result.map(a => {})
+            res.status(200).json(result)
+            })
+      }
+    });
 });
 
 module.exports = router;
