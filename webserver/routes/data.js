a3e7aebcff352599fb3a468b704d7994228db6fc
Joe Savage <joe.savage@gmail.com>
2019-09-23 08:11:40 -0500

    refactor db connection

--- webserver/routes/data.js
+++ webserver/routes/data.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
 
-var conn = mysql.createConnection({
+var pool = mysql.createPool({
+    connectionLimit: 100,
     host: "localhost",
     user: "bbq",
     password: "bbq",
@@ -12,23 +13,35 @@ var conn = mysql.createConnection({
 
 /* GET data page. */
 router.get('/', function(req, res, next) {
-    secs = req.query.sec
-  conn.connect(function(err) {
-      if (err){
-          res.status(200).json({})
-      }
-      else {
-        let date = Math.floor(new Date() / 1000)
-        let tzOffset = (new Date()).getTimezoneOffset() * 60
-        let localTime = (date - tzOffset)
-        let startDate = (localTime) - secs
-        let select = `SELECT * from pid_view where local_time > ${startDate} ORDER BY time asc`
-        console.log(select)
-        conn.query(select, function (err, result, fields){
-            data = result.map(a => {})
-            res.status(200).json(result)
-            })
-      }
+    let secs = 3600
+    if (req.query.sec) {
+        secs = parseInt(req.query.sec)
+    }
+    console.log(`secs = ${secs}`)
+    pool.getConnection(function(err, connection) {
+        if (err){
+            console.log(err)
+            res.status(200).json({error: err})
+        }
+        else {
+            let date = Math.floor(new Date() / 1000)
+            let tzOffset = (new Date()).getTimezoneOffset() * 60
+            let localTime = (date - tzOffset)
+            let startDate = (localTime) - secs
+            let select = `SELECT * from pid_view where local_time > ${startDate} ORDER BY time asc`
+            console.log(select)
+            connection.query(select, function (err, result, fields){
+                if (err) {
+                    console.log(err);
+                    res.status(200).json({error: err})
+                }
+                else {
+                    data = result.map(a => {})
+                    res.status(200).json(result)
+                }
+            });
+            connection.release()
+        }       
     });
 });
 
