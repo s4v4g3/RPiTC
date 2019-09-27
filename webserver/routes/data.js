813d0c3cca1c06fc5b2cc297f2e1ae16a35d0e70
Joe Savage <joe.savage@gmail.com>
2019-09-26 19:11:22 -0500

    added performance timer

--- webserver/routes/data.js
+++ webserver/routes/data.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
+const {
+    performance,
+  } = require('perf_hooks');
 
 var pool = mysql.createPool({
     connectionLimit: 100,
@@ -17,7 +20,9 @@ router.get('/', function(req, res, next) {
     if (req.query.sec) {
         secs = parseInt(req.query.sec)
     }
-    console.log(`secs = ${secs}`)
+    console.log(`getting ${secs} secs of data from database `)
+    var t0 = performance.now();
+
     pool.getConnection(function(err, connection) {
         if (err){
             console.log(err)
@@ -36,7 +41,9 @@ router.get('/', function(req, res, next) {
                     res.status(200).json({error: err})
                 }
                 else {
-                    data = result.map(a => {})
+                    //data = result.map(a => {})
+                    var t1 = performance.now();
+                    console.log("database query took " + (t1 - t0) + " milliseconds.");
                     res.status(200).json(result)
                 }
             });
