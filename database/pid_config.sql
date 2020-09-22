16fa833a81eec002284a8e7bd97b8ed0ccb7cc71
Joe Savage <joe.savage@gmail.com>
2020-09-22 11:07:40 -0500

    fixed sql files for postgres

--- database/pid_config.sql
+++ database/pid_config.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `pid_config` (
-  `id` char(36) NOT NULL,
-  `set_point` int(11) DEFAULT NULL,
-  `kb` float DEFAULT NULL,
-  `kp` float DEFAULT NULL,
-  `ki` float DEFAULT NULL,
-  `kd` float DEFAULT NULL,
-  `smooth_coeff` float DEFAULT NULL,
-  `max_output` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='			';
+CREATE TABLE pid_config (
+  id char(36) NOT NULL,
+  set_point int(11) DEFAULT NULL,
+  kb float DEFAULT NULL,
+  kp float DEFAULT NULL,
+  ki float DEFAULT NULL,
+  kd float DEFAULT NULL,
+  smooth_coeff float DEFAULT NULL,
+  max_output float DEFAULT NULL,
+  PRIMARY KEY id
+)
